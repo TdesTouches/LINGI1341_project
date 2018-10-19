779fc2a8690031ce59b83e25fefcdbc91f3a9f63
gennartan <gennart.antoine@gmail.com>
2018-10-19 11:18:50 +0200

    time function added

--- src/utils.c
+++ src/utils.c
@@ -1,8 +1,41 @@
+#include <stdio.h>
+#include <sys/timeb.h>
+
+#include "pkt.h"
+
 #include "utils.h"
 
-void slide_array(void *array, int len){
+
+void slide_array(pkt_t **array, int len){
 	int i=0;
 	for(i=0; i<len-1; i++){
 		array[i] = array[i+1];
 	}
+}
+
+
+uint32_t file_size(FILE* f){
+	fseek(f, 0L, SEEK_END);
+	uint32_t sz = ftell(f);
+	rewind(f);
+	return sz;
+}
+
+
+uint32_t tot_nb_packet(FILE *f){
+	uint32_t nb_packet = 0;
+	uint32_t nb_bytes = file_size(f);
+	nb_packet += nb_bytes / 512;
+	if(nb_bytes % 512 != 0){
+		nb_packet += 1;
+	}
+	return nb_packet;
+}
+
+
+uint32_t get_time(){
+	struct timeb tp;
+	ftime(&tp);
+	uint32_t ct = (uint32_t) tp.time * 1000 + (uint32_t) tp.millitm;
+	return ct;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,21 @@
 #ifndef __UTILS_H_
 #define __UTILS_H_
 
+/*
+ * slide array to the left
+ */
+void slide_array(pkt_t **array, int len);
 
-void slide_array(void *array);
+/*
+ * 
+ */
+uint32_t file_size(FILE* f);
+
+/*
+ *
+ */
+uint32_t tot_nb_packet(FILE *f);
+
+uint32_t get_time();
 
 #endif
