c51ddd5d7ea7cd542ca3ee6c2687e9691069f510
gennartan <gennart.antoine@gmail.com>
2018-10-20 10:01:19 +0200

    simplete test

---
+++ tests/simple_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+../receiver ::1 1234 > fichier.dat 2> log_receiver.log &
+sleep 3
+../sender -f ../src/sender.c ::1 1234 > log_sender.log
+sleep 3
+diff -s fichier.dat ../src/sender.c
