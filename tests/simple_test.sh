d3e91c884fd3d34c701e92f9c35db6de42ef4d84
gennartan <gennart.antoine@gmail.com>
2018-10-26 23:19:34 +0200

    create random file for different size testing

--- tests/simple_test.sh
+++ tests/simple_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# truncate -s 131073 input.dat
+head -c 131073 /dev/urandom > input.dat
 valgrind ./receiver ::1 12344 1> fichier.dat 2> log_receiver.log &
 sleep 3
-valgrind ./sender -f src/sender.c ::1 12344 2> log_sender.log
+valgrind ./sender -f input.dat ::1 12344 2> log_sender.log
 sleep 3
-diff -s fichier.dat src/sender.c
+diff -s fichier.dat input.dat
--- tests/simple_test_losseau.sh
+++ tests/simple_test_losseau.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+rm -f input.dat
+head -c 131073 /dev/urandom > input.dat
 valgrind ./receiver ::1 1234 1> fichier.dat 2> log_receiver.log &
 sleep 3
-valgrind ./sender_losseau -f src/sender.c ::1 1234 2> log_sender.log
+valgrind ./sender_losseau -f input.dat ::1 1234 2> log_sender.log
 sleep 3
-diff -s fichier.dat src/sender.c
+diff -s fichier.dat input.dat
